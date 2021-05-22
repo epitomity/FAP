d11526d9b5482b5b3a16773aecc642f9262a8be0
jim <jim@epitomity>
2021-05-23 01:19:50 +0800

    new commit

--- src/java/Person.java
+++ src/java/Person.java
@@ -13,6 +13,58 @@ public class Person {
     private String username;
     private String password;
     private String role;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String company;
+    private int postalCode;
+    private String region;
+    private String country;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     public String getUsername() {
         return username;
@@ -26,15 +78,37 @@ public class Person {
         return role;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    public String getEmail() {
+        return email;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+    
+    
 }
